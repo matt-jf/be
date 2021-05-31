bb04132795ccd33a561524f1e3280311d2d744da
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-05-31 19:31:23 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,3 @@
 <?php
-	$mysqli = new mysqli('localhost', 'root', '', 'be_db');
-	if(!$koneksi){
-		die ("Koneksi dengan DB gagal: ".mysql_connect_error());
-	}
+	$koneksi = new mysqli('localhost', 'root', '', 'be_db');
 ?>
