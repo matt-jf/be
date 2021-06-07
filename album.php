960e6c333c86177c5de698e6d2739663c9ebb1e1
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-07 21:09:13 +0700

    Update album.php

--- album.php
+++ album.php
@@ -1,59 +1,55 @@
-<?
-
-session_start();
-
-if ( !isset($_SESSION["login"]) ) {
-    header("Location: login.php");
-    exit;
-}
+<?php
+  ini_set('error_reporting()', E_ALL);
+  ini_set('display_errors', true);
+  include('koneksi.php');
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Redressed" />
-    <title>Album</title>
-    <style type="text/css">
-    .container{
-       margin-left:10%;
-       
-    }
-    * {
-        font-family:'Redressed';
-      }
+    <title>Album | Billie Eilish</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Redressed&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Reem+Kufi&display=swap" rel="stylesheet">
     
+    <style type="text/css">    
     body{
         background-color: black;
-          height: 100%px;
+        font-family: 'Reem Kufi';
+        color: white;
+          height: 100%;
           width: 100%;
 
-      }  
-      
+      }
+
     .topnav {
+      width: 100%;
       margin-top: -10px;
       margin-left:-10px;
+        font-family: 'Redressed';
   overflow: hidden;
-  background-color: #8E6F55;
+  background-image: url("gold_bg.png");
 }
 
 .topnav a {
   float: left;
   display: block;
-  color: #442813;
+  color: #f2f2f2;
   text-align: center;
-  padding: 14px 16px;
+  padding: 8px 10px;
   text-decoration: none;
   font-size: 17px;
-  background-color:#8E6F55;
+  background-image: url("gold_bg.png");
 }
 
 .topnav a:hover {
   background-color: #ddd;
-  color: #8E6F55;
+  color: black;
 }
 
 .topnav a.active {
- 
-  color: white;
+  background-image: url("gold_bg.png");
+  color: #D0BBA2;
 }
 
 .topnav .icon {
@@ -62,7 +58,7 @@ if ( !isset($_SESSION["login"]) ) {
 .topnav-right {
   float: right;
 }
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 200%) {
   .topnav a:not(:first-child) {display: none;}
   .topnav a.icon {
     float: right;
@@ -70,7 +66,7 @@ if ( !isset($_SESSION["login"]) ) {
   }
 }
 
-@media screen and (max-width: 600px) {
+@media screen and (max-width: 200%) {
   .topnav.responsive {position: relative;}
   .topnav.responsive .icon {
     position: absolute;
@@ -83,49 +79,34 @@ if ( !isset($_SESSION["login"]) ) {
     text-align: left;
   }
 }
-div.gallery {
-  margin: 30px;
-  border: 1px solid ;
-  float: left;
-  width: 40%;
-  font-size : 40px;
-}
-
-div.gallery:hover {
-  border: 1px solid #777;
-}
-
-div.gallery img {
-  width: 100%;
-  height: auto;
-}
-
-div.desc {
-  padding: 15px;
-  text-align: center;
+.grid-container{
+  display: grid;
+  padding: 2%;
+  grid-gap: 5%;
+  align-content: center;
+  grid-template-columns: 45% 45%;
 }
 .footer {
   position: static;
   right: 0;
   bottom: 0;
   left: 0;
-  padding: 1rem;
+  padding: 1 rem;
   text-align: center;
-  color:black;
+  color:white;
 }
-
     </style>
   </head>
   <body class="h-vh-100 bg-brandColor2">
   <div class="topnav" id="myTopnav">
-  <a style="color: white;">Billie Eilish</a>
+  <a style="color: #D0BBA2; font-size: 140%; font-weight: 6;">Billie Eilish</a>
   <div class="topnav-right">
-  <a href="home.php">Home</a>
-  <a href="about.php">About</a>
-  <a class="active" href="album.php">Album</a>
-  <a href="galery.php">Galery</a>
-  <a href="tour.php">Tour</a>
-    <a href="logout.php">Logout</a>
+  <a style="color: #442813;font-size: 130%;"href="home.php">Home</a>
+  <a style="color: #442813;font-size: 130%;" href="about.php">About</a>
+  <a class="active" style="font-size: 130%;" href="album.php">Album</a>
+  <a style="color: #442813;font-size: 130%;" href="galery.php">Galery</a>
+  <a style="color: #442813;font-size: 130%;" href="tour.php">Tour</a>
+  <a style="color: #442813;font-size: 130%;" href="logout.php">Logout</a>
   </div>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
@@ -141,39 +122,28 @@ function myFunction() {
   }
 }
 </script>
-<div class="container">
-<div class="gallery">
-    <img src="happier_than_ever.jpg"  width="600" height="400">
-  <div class="desc">
-  <a target="_blank"style="color: white;" href="https://www.youtube.com/watch?v=66TYJJObd6E">Happier Than Ever</a>
+<div class="container" align="center">
+  <?php
+  $query = "SELECT * FROM album ORDER BY id_album DESC";
+  $result = mysqli_query($koneksi, $query);
+  if(!$result){
+    die ("Query Error: ".mysqli_errno($koneksi).
+       " - ".mysqli_error($koneksi));
+  }
+  ?>
+  <div class="grid-container">
+  <?php
+  while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+  <div class="item">
+    <img width="80%" src="<?php echo $row['art_album']; ?>">
+    <br>
+    <div class="desc"><a style = "color: white;font-size: 120%;" href="<?php echo $row['link_album']; ?>"><?php echo $row['name_album']; ?></a></div>
   </div>
+  <?php } ?>
 </div>
-
-<div class="gallery">
-    <img src="Live_at_Third_Man_Records.jpg"  width="600" height="400">
-  <div class="desc">
-  <a target="_blank" style="color: white;" href="https://www.youtube.com/watch?v=9F_YUo_UwG4">LIVE AT THIRD MAN RECORDS</a>
-  </div>
 </div>
-<div class="gallery">
-    <img src="When_We_All_Fall_Asleep,_Where_Do_We_Go.png"  width="600" height="400">
-  <div class="desc">
-  <a target="_blank" style="color: white;" href="https://www.youtube.com/watch?v=XJBUWZsT38c&list=OLAK5uy_kQbMBLWm9sUrVNnooayU8ilISrC7KintI">When We All Fall Asleep, Where Do We Go?</a>
-  </div>
-</div>
-
-<div class="gallery">
-    <img src="Don't_Smile_at_Me.png"  width="600" height="400">
-  <div class="desc">
-  <a target="_blank"style="color: white;" href="https://www.youtube.com/watch?v=ebb5AinKxWI&list=OLAK5uy_mYmH6NFnJ6IaH3Ln3wGeVimrFk8FXKzcA">Don't Smile at Me</a>
-  </div>
-</div>
-  </div>
-</div>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-
+<br>
 <div class="footer"><strong>© SNEE</strong></div>
-
 </body>
 </html>
