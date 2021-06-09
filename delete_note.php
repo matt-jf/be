41975bb548b51cbd98efac5574e387f85fdbf7c7
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-09 11:10:52 +0700

    Update delete_note.php

--- delete_note.php
+++ delete_note.php
@@ -1,12 +1,11 @@
 <?php
-include 'koneksi.php';
-$id_note = $_POST['id_note'];
-    $query = "DELETE FROM note WHERE id_note = $id_note";
-    $hasil_query = mysqli_query($koneksi, $query);
-
-    if(!$hasil_query) {
-      die ("Gagal menghapus data: ".mysqli_errno($koneksi).
-       " - ".mysqli_error($koneksi));
-    } else {
-      echo "<script>alert('Data berhasil dihapus.');window.location='home.php';</script>";
-    }
+  include 'koneksi.php';
+  $id_note = $_POST['id_note'];
+  $query = "DELETE FROM note WHERE id_note = $id_note";
+  $hasil_query = mysqli_query($koneksi, $query);
+  if(!$hasil_query) {
+    die ("Gagal menghapus data: ".mysqli_errno($koneksi)." - ".mysqli_error($koneksi));
+  } else {
+    echo "<script>alert('Data berhasil dihapus.');window.location='home.php';</script>";
+  }
+?>
