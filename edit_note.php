f912b69358a4292e62fd5d2918e1b3b954e475fc
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-09 11:18:55 +0700

    Update edit_note.php

--- edit_note.php
+++ edit_note.php
@@ -1,16 +1,16 @@
 <?php
-$id_note = $_GET['id_note'];
-if (!$id_note) {
-    header('Location: home.php');
-    die;
-}
-include_once 'koneksi.php';
-$query = "SELECT * FROM note WHERE id_note = $id_note";
-$result = $koneksi->query($query);
-if (!$result->num_rows) {
-    header('Location: home.php');
-}
-$note = $result->fetch_object();
+    $id_note = $_GET['id_note'];
+    if (!$id_note) {
+        header('Location: home.php');
+        die;
+    }
+    include_once 'koneksi.php';
+    $query = "SELECT * FROM note WHERE id_note = $id_note";
+    $result = $koneksi->query($query);
+    if (!$result->num_rows) {
+        header('Location: home.php');
+    }
+    $note = $result->fetch_object();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,72 +30,77 @@ $note = $result->fetch_object();
             margin:10px;
             background: #ededed;  
         }
+
         input {
-      padding: 6px;
-      width: 100%;
-      box-sizing: border-box;
-      background: #f8f8f8;
-      border: 2px solid #ccc;
-      outline-color: #328fa8;
-    }
-    textarea{
-        padding: 6px;
-      width: 100%;
-      height: 400px;
-      box-sizing: border-box;
-      background: #f8f8f8;
-      border: 2px solid #ccc;
-      outline-color: #328fa8;
-    }
-    label {
-      margin-top: 10px;
-      float: left;
-      text-align: left;
-      width: 100%;
-    }
-         body{
-          background-image: url("gold_bg.png");
-          text-align: center;
-          height: 100%;
-          width: 100%;
-          font-family: "Redressed";
-      } 
-      h1 {
-        text-transform: uppercase;
-        color: white;
-      }
-      button {
-          background-color: #328fa8;
-          color: #fff;
-          padding: 10px;
-          text-decoration: none;
-          font-size: 12px;
-          border: 0px;
-          margin-top: 20px;
-    }
+            padding: 6px;
+            width: 100%;
+            box-sizing: border-box;
+            background: #f8f8f8;
+            border: 2px solid #ccc;
+            outline-color: #328fa8;
+        }
+
+        textarea {
+            padding: 6px;
+            width: 100%;
+            height: 400px;
+            box-sizing: border-box;
+            background: #f8f8f8;
+            border: 2px solid #ccc;
+            outline-color: #328fa8;
+        }
+
+        label {
+            margin-top: 10px;
+            float: left;
+            text-align: left;
+            width: 100%;
+        }
+
+        body {
+            background-image: url("gold_bg.png");
+            text-align: center;
+            height: 100%;
+            width: 100%;
+            font-family: "Redressed";
+        }
+
+        h1 {
+            text-transform: uppercase;
+            color: white;
+        }
+
+        button {
+            background-color: #442813;
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            font-size: 12px;
+            border: 0px;
+            margin-top: 20px;
+        }
     </style>
 </head>
 <body>
-<center>
-        <h1>Edit Note</h1>
-      <center>
-<div class="container">
-    <?php if (isset($_GET['invalid'])): ?>
+    <center><h1>Edit Note</h1><center>
+    <div class="container">
+        <?php if (isset($_GET['invalid'])): ?>
         <h3>Please complete the form</h3>
-    <?php endif; ?>
-    <form action="edit_note_process.php" method="post">
-        <input type="hidden" name="id_note" value="<?php echo $note->id_note; ?>">
-        <label>
-        Title:
-        <input type="text" name="title_note" value="<?php echo $note->title_note; ?>" autofocus>
-        </label>
-        <br>
-        <label>
-        Content:
-        <textarea name="content_note" cols="30" rows="10" ><?php echo $note->content_note; ?></textarea>
-        </label>
-        <br>
-        <button type="submit">Save Changes</button>
-    </form>
+        <?php endif; ?>
+        <form action="edit_note_process.php" method="post">
+            <input type="hidden" name="id_note" value="<?php echo $note->id_note; ?>">
+            <label>
+                Title:
+                <input type="text" name="title_note" value="<?php echo $note->title_note; ?>" autofocus>
+            </label>
+            <br>
+            <label>
+                Content:
+                <textarea name="content_note" cols="30" rows="10" ><?php echo $note->content_note; ?></textarea>
+            </label>
+            <br>
+            <button type="submit">Save Changes</button>
+        </form>
+    </div>
 </body>
 </html>
