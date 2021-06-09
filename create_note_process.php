93ec328db07b517f43651fd6eca0c26959111860
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-09 11:08:29 +0700

    Update create_note_process.php

--- create_note_process.php
+++ create_note_process.php
@@ -1,11 +1,12 @@
 <?php
-$title_note = addslashes(trim($_POST['title_note']));
-$content_note = addslashes(trim($_POST['content_note']));
-if (!$title_note || !$content_note) {
-    header('Location: create_note.php?invalid=1');
-    die;
-}
-include_once 'koneksi.php';
-$query = "INSERT INTO note (title_note, content_note) VALUES ('$title_note', '$content_note')";
-$koneksi->query($query);
-header("Location: home.php");
+    $title_note = addslashes(trim($_POST['title_note']));
+    $content_note = addslashes(trim($_POST['content_note']));
+    if (!$title_note || !$content_note) {
+        header('Location: create_note.php?invalid=1');
+        die;
+    }
+    include_once 'koneksi.php';
+    $query = "INSERT INTO note (title_note, content_note) VALUES ('$title_note', '$content_note')";
+    $koneksi->query($query);
+    header("Location: home.php");
+?>
