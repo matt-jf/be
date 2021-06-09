63a8ff6a476dbadd2e9c4db684f05c3c521cad69
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-09 11:09:51 +0700

    Update delete_galery.php

--- delete_galery.php
+++ delete_galery.php
@@ -1,12 +1,11 @@
 <?php
-include 'koneksi.php';
-$id_galery = $_POST['id_galery'];
-    $query = "DELETE FROM galery WHERE id_galery = $id_galery";
-    $hasil_query = mysqli_query($koneksi, $query);
-
-    if(!$hasil_query) {
-      die ("Gagal menghapus data: ".mysqli_errno($koneksi).
-       " - ".mysqli_error($koneksi));
-    } else {
-      echo "<script>alert('Data berhasil dihapus.');window.location='galery.php';</script>";
-    }
+  include 'koneksi.php';
+  $id_galery = $_POST['id_galery'];
+  $query = "DELETE FROM galery WHERE id_galery = $id_galery";
+  $hasil_query = mysqli_query($koneksi, $query);
+  if(!$hasil_query) {
+    die ("Gagal menghapus data: ".mysqli_errno($koneksi)." - ".mysqli_error($koneksi));
+  } else {
+    echo "<script>alert('Data berhasil dihapus.');window.location='galery.php';</script>";
+  }
+?>
