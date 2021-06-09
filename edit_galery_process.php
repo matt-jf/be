74ea521a1bf481dd58d77b489885b4b03d123ec7
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-09 11:15:14 +0700

    Update edit_galery_process.php

--- edit_galery_process.php
+++ edit_galery_process.php
@@ -1,13 +1,13 @@
 <?php
-include_once 'koneksi.php';
-$id_galery = $_POST['id_galery'];
-$name_galery = addslashes(trim($_POST['name_galery']));
-$photo_galery = addslashes(trim($_POST['photo_galery']));
-if (!$name_galery || !$photo_galery) {
-    header("Location: edit_galery.php?id_galery=$id_galery&invalid=1");
-    die;
-}
-$query = "UPDATE galery SET name_galery = '$name_galery', photo_galery =
-'$photo_galery' WHERE id_galery = $id_galery";
-$koneksi->query($query);
-header('Location:galery.php');
+    include_once 'koneksi.php';
+    $id_galery = $_POST['id_galery'];
+    $name_galery = addslashes(trim($_POST['name_galery']));
+    $photo_galery = addslashes(trim($_POST['photo_galery']));
+    if (!$name_galery || !$photo_galery) {
+        header("Location: edit_galery.php?id_galery=$id_galery&invalid=1");
+        die;
+    }
+    $query = "UPDATE galery SET name_galery = '$name_galery', photo_galery ='$photo_galery' WHERE id_galery = $id_galery";
+    $koneksi->query($query);
+    header('Location:galery.php');
+?>
