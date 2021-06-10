85bb245e150c2f98853feb4b6a4759102ab7e935
NikmatusYuliaFatimah18510400111049 <74701406+matt-jf@users.noreply.github.com>
2021-06-10 07:04:01 +0700

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -1,21 +1,22 @@
 <?php
-include_once 'koneksi.php';
-$username = $_POST['username'];
-$password = $_POST['password'];
-$sql = "SELECT * FROM user WHERE username = '$username'";
-        $result = $koneksi->query($sql);
-        if ($result->num_rows) {
-            $row = $result->fetch_object();
-            if (password_verify($password, $row->password)) {
-                session_start();
-                $_SESSION['app123_logged_in'] = true;
-                $_SESSION['name'] = $row->name;
-                header('Location: home.php');
-            } else {
-                echo 'Wrong username or password.';
-            }
+    include_once 'koneksi.php';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM user WHERE username = '$username'";
+    $result = $koneksi->query($sql);
+    if ($result->num_rows) {
+        $row = $result->fetch_object();
+        if (password_verify($password, $row->password)) {
+            session_start();
+            $_SESSION['app123_logged_in'] = true;
+            $_SESSION['name'] = $row->name;
+            header('Location: home.php');
         } else {
             echo 'Wrong username or password.';
         }
-$result->close();
-$koneksi->close();
+    } else {
+        echo 'Wrong username or password.';
+    }
+    $result->close();
+    $koneksi->close();
+?>
